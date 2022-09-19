ee70d693cc7d91cd35a04a37c080f86d0ea80e37
yeungadrian <yeungadrianjy@gmail.com>
2022-09-19 20:05:41 +0100

    Updating readme & tests

--- src/tests/test_factor.py
+++ src/tests/test_factor.py
@@ -45,3 +45,21 @@ def test_apple_regression_12month():
     assert response.json()[0]["coefficient"]["HML"] == pytest.approx(
         -2.0743626259033987
     )
+
+
+def test_rolling_regression():
+    response = client.post(
+        "/factor_analysis/rolling/",
+        json={
+            "start_date": "2017-12-31",
+            "end_date": "2019-12-31",
+            "funds": ["AAPL"],
+            "factors": ["MktRF", "SMB", "HML"],
+            "frequency": "monthly",
+        },
+    )
+    assert response.json()[0]["fund_code"] == "AAPL"
+    assert response.json()[0]["params"] ["Intercept"]["2019-12-31"]== pytest.approx(0.0158573968)
+    assert response.json()[0]["params"] ["MktRF"]["2019-12-31"]== pytest.approx(1.7490203081)
+    assert response.json()[0]["params"] ["SMB"]["2019-12-31"]== pytest.approx(-1.4669490872)
+    assert response.json()[0]["params"] ["HML"]["2019-12-31"]== pytest.approx(0.4853368407)
